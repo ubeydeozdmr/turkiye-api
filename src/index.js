748314a7ec45f8f99e11b22a573b5e57bcf73cc0
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-09-18 00:00:49 +0300

    Design of the documentation has renewed completely

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,12 +9,21 @@ const app = express();
 const PORT = process.env.PORT || 8181;
 
 app.set('view engine', 'pug');
+app.use(express.static('assets'));
+app.use(express.static('favicon'));
 
 app.use(cors());
 app.options('*', cors());
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
-  res.render('index', { host: req.headers.host });
+  res.render('index', {
+    host: req.headers.host,
+    protocol: process.env.NODE_ENV == 'production' ? 'https://' : '',
+    image:
+      fs.readdirSync('assets')[
+        Math.floor(Math.random() * fs.readdirSync('assets').length)
+      ],
+  });
 });
 app.use('/api/v1/provinces', v1ProvinceRouter);
 
