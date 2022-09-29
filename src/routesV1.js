731e1a7dd0b8bb55e19f3459db733ab78dae325f
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-09-29 23:59:05 +0300

    Some improvements

--- src/routesV1.js
+++ src/routesV1.js
@@ -7,6 +7,12 @@ router
   .get('/provinces', controller.getProvinces)
   .get('/provinces/:id', controller.getExactProvince)
   .get('/districts', controller.getDistricts)
-  .get('/districts/:id', controller.getExactDistrict);
+  .get('/districts/:id', controller.getExactDistrict)
+  .get('*', (req, res) => {
+    res.json({
+      status: 'ERROR',
+      error: 'Wrong endpoint.',
+    });
+  });
 
 module.exports = router;
