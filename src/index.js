fd2851d4147529bdb9c0f0f032ccace2d35574d2
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-02-20 00:29:46 +0300

    Updates and small changes

--- src/index.js
+++ src/index.js
@@ -2,44 +2,41 @@ const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
 const express = require('express');
-const bodyParser = require('body-parser');
 const v1Router = require('./routesV1');
 require('./helpers/localizer');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 8181;
-const { NODE_ENV } = process.env;
+const { NODE_ENV, PORT } = process.env;
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.options('*', cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  const images = fs.readdirSync(path.join(__dirname, 'public/assets'));
-  res.render('index', {
+app.use((req, res, next) => {
+  res.locals = {
     host: req.get('host'),
     protocol: req.get('x-forwarded-proto') ? 'https://' : 'http://',
-    image: images[Math.floor(Math.random() * images.length)],
-  });
+  };
+  next();
+});
+
+app.get('/', (req, res) => {
+  const images = fs.readdirSync(path.join(__dirname, 'public', 'assets'));
+  res.locals.image = images[Math.floor(Math.random() * images.length)];
+  res.render('index', res.locals);
 });
 
 app.get('/docs', (req, res) => {
-  res.render('docs', {
-    host: req.get('host'),
-    protocol: req.get('x-forwarded-proto') ? 'https://' : 'http://',
-  });
+  res.render('docs', res.locals);
 });
 
 app.get('/examples', (req, res) => {
-  res.render('examples', {
-    host: req.get('host'),
-    protocol: req.get('x-forwarded-proto') ? 'https://' : 'http://',
-  });
+  res.render('examples', res.locals);
 });
 
 app.use('/api/v1', v1Router);
@@ -48,7 +45,7 @@ app.all('*', (req, res, next) => {
   res.render('notfound');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT || 8181, () => {
   console.log(`API is listening on port ${PORT}`);
   if (NODE_ENV === 'development') console.log(`http://localhost:${PORT}`);
 });
