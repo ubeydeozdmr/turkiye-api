f559b999b07e0febf8b0b2328ab3bae8246caed6
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-03-05 00:03:30 +0300

    Revert "Updates and small changes"
    This reverts commit fd2851d4147529bdb9c0f0f032ccace2d35574d2.

--- src/index.js
+++ src/index.js
@@ -2,41 +2,44 @@ const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
 const express = require('express');
+const bodyParser = require('body-parser');
 const v1Router = require('./routesV1');
 require('./helpers/localizer');
 require('dotenv').config();
 
 const app = express();
-const { NODE_ENV, PORT } = process.env;
+const PORT = process.env.PORT || 8181;
+const { NODE_ENV } = process.env;
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.options('*', cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use((req, res, next) => {
-  res.locals = {
-    host: req.get('host'),
-    protocol: req.get('x-forwarded-proto') ? 'https://' : 'http://',
-  };
-  next();
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  const images = fs.readdirSync(path.join(__dirname, 'public', 'assets'));
-  res.locals.image = images[Math.floor(Math.random() * images.length)];
-  res.render('index', res.locals);
+  const images = fs.readdirSync(path.join(__dirname, 'public/assets'));
+  res.render('index', {
+    host: req.get('host'),
+    protocol: req.get('x-forwarded-proto') ? 'https://' : 'http://',
+    image: images[Math.floor(Math.random() * images.length)],
+  });
 });
 
 app.get('/docs', (req, res) => {
-  res.render('docs', res.locals);
+  res.render('docs', {
+    host: req.get('host'),
+    protocol: req.get('x-forwarded-proto') ? 'https://' : 'http://',
+  });
 });
 
 app.get('/examples', (req, res) => {
-  res.render('examples', res.locals);
+  res.render('examples', {
+    host: req.get('host'),
+    protocol: req.get('x-forwarded-proto') ? 'https://' : 'http://',
+  });
 });
 
 app.use('/api/v1', v1Router);
@@ -45,7 +48,7 @@ app.all('*', (req, res, next) => {
   res.render('notfound');
 });
 
-app.listen(PORT || 8181, () => {
+app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
   if (NODE_ENV === 'development') console.log(`http://localhost:${PORT}`);
 });
